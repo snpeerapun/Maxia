c4e351469c1e6ba4cd548c6b1f07598de1caa9bb
Peerapun8403 <snpeerapun@hotmail.com>
2023-08-13 18:20:15 +0700

    33

--- mic.py
+++ mic.py
@@ -1,16 +1,11 @@
-import time
 import speech_recognition as sr
 from gtts import gTTS
-from io import BytesIO
-import pygame
-
 import pygame
 from io import BytesIO
-from gtts import gTTS
 
 class TextToSpeech:
-    @classmethod
-    def speak(cls, text):
+    @staticmethod
+    def speak(text):
         mp3_file_object = BytesIO()
         tts = gTTS(text, lang='en')
         tts.write_to_fp(mp3_file_object)
@@ -28,25 +23,25 @@ class TextToSpeech:
         pygame.mixer.music.stop()
         pygame.quit()
 
-
 def main():
     recognizer = sr.Recognizer()
-    
-    with sr.Microphone() as source:
-        print("Say something...")
-        audio = recognizer.listen(source)
 
-    try:
-        recognized_text = recognizer.recognize_google(audio)
-        print(f"You said: {recognized_text}")
-        
-        response_text = f"You said: {recognized_text}"
-        TextToSpeech.speak(response_text)
-        
-    except sr.UnknownValueError:
-        print("Sorry, could not understand audio.")
-    except sr.RequestError as e:
-        print(f"Error connecting to the Google Web Speech API: {e}")
+    while True:
+        with sr.Microphone() as source:
+            print("Say something...")
+            audio = recognizer.listen(source)
+
+        try:
+            recognized_text = recognizer.recognize_google(audio)
+            print(f"You said: {recognized_text}")
+
+            response_text = f"You said: {recognized_text}"
+            TextToSpeech.speak(response_text)
+
+        except sr.UnknownValueError:
+            print("Sorry, could not understand audio.")
+        except sr.RequestError as e:
+            print(f"Error connecting to the Google Web Speech API: {e}")
 
 if __name__ == "__main__":
     main()
